5d40c9039fc2bc0bef076319734517dcd8dec268
최원영 <choewy32@gmail.com>
2026-01-26 13:32:13 +0900

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    plugins: {
+      'simple-import': simpleImportSort,
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'simple-import/imports': [
+        'error',
+        {
+          groups: [['^@nestjs(/.*|$)'], ['^node:', '^(?!@nestjs)(@?\\w)'], ['^@libs(/.*|$)'], ['^@apps(/.*|$)'], ['^\\.\\./'], ['^\\./']],
+        },
+      ],
+    },
+  },
+);
