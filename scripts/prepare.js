2c1f54b0f526117906f3fdd7b0c423d0e39e7578
최원영 <choewy32@gmail.com>
2026-01-28 01:26:07 +0900

    chore: prepare.js

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -4,7 +4,7 @@ const { spawnSync } = require('node:child_process');
 
 const ROOT = path.resolve(__dirname, '..');
 
-function prepare() {
+function setEnvRoot() {
   const envPath = path.resolve(ROOT, '.env');
   const envLocalPath = path.resolve(ROOT, '.env.local');
 
@@ -19,9 +19,30 @@ function prepare() {
   fs.cpSync(envLocalPath, envPath);
 }
 
+function setEnvApps() {
+  const appsPath = path.resolve(ROOT, 'apps');
+  const apps = fs.readdirSync(appsPath);
+
+  for (const app of apps) {
+    const envPath = path.resolve(ROOT, 'apps', app, '.env');
+    const envLocalPath = path.resolve(ROOT, 'apps', app, '.env.local');
+
+    if (!fs.existsSync(envLocalPath)) {
+      continue;
+    }
+
+    if (fs.existsSync(envPath)) {
+      continue;
+    }
+
+    fs.cpSync(envLocalPath, envPath);
+  }
+}
+
 function husky() {
   spawnSync('pnpx', ['husky']);
 }
 
-prepare();
+setEnvRoot();
+setEnvApps();
 husky();
